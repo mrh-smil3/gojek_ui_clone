fe5d5dd1cfbccddd37e0086c0ea07cc38cdc528d
ARIF <50254637+mrh-smil3@users.noreply.github.com>
2024-05-05 08:43:32 +0700

    Common Bug Fix

--- lib/common/my_style.dart
+++ lib/common/my_style.dart
@@ -1,26 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:indojek/common/my_colors.dart';
+import 'package:gojek_ui_clone/common/my_colors.dart';
 
 import 'my_font_size.dart';
 
 class MyStyle {
   static TextStyle textButtonBlack = TextStyle(
-    color: MyColors.blackText,
-    fontSize: MyFontSize.medium2,
-    fontWeight: FontWeight.bold
-  );
+      color: MyColors.blackText,
+      fontSize: MyFontSize.medium2,
+      fontWeight: FontWeight.bold);
 
   static TextStyle textButtonWhite = TextStyle(
-    color: MyColors.white,
-    fontSize: MyFontSize.medium2,
-    fontWeight: FontWeight.bold
-  );
+      color: MyColors.white,
+      fontSize: MyFontSize.medium2,
+      fontWeight: FontWeight.bold);
 
   static TextStyle textTitleBlack = TextStyle(
-    color: MyColors.blackText,
-    fontSize: MyFontSize.large2,
-    fontWeight: FontWeight.bold
-  );
+      color: MyColors.blackText,
+      fontSize: MyFontSize.large2,
+      fontWeight: FontWeight.bold);
 
   static TextStyle textParagraphBlack = TextStyle(
     color: MyColors.blackSoftText,
